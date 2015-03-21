a777f01b2d10ca7b0b52cdd85ce5f44fbab79dc8
campusCoder <campusCoder@users.noreply.github.com>
2015-03-21 03:36:37 +0200

    function for channel chat as a module and works

---
+++ client_side/chanMsg.c
@@ -0,0 +1,37 @@
+#include "client.h"
+
+
+
+//Handling client's channel join command message
+void chanMessageHandle(int connfd, char *send, char *recv, char *getCmd, Msgheader chatheader){
+
+int n;
+
+	if(pass_message(connfd, getCmd, &chatheader)<0){
+		printf("there was problem connecting to a chat client\n");
+	}
+		printf("%s: ",chatheader.sender_id);
+		memset(send, 0, sizeof(send));
+		while (fgets(send, MAXMSG, stdin) != NULL) {
+					
+			printf("%s: ",chatheader.sender_id);
+			//always send the string length of the sent msg
+	        chatheader.msglen=strlen(send); 
+			if (pass_message(connfd, send, &chatheader)<0) {
+				perror("sendto");
+				return -1;
+			}
+					
+			printf("%s: ",chatheader.recipient_id);
+			if ((n = read_message(connfd, recv, &chatheader))<0) {
+				perror("recvfrom");
+				return -1;
+			}
+	
+			recv[n] = 0;        // null terminate 
+			if (fputs(recv, stdout) == EOF) {
+				fprintf(stderr, "fputs error\n");
+				return -1;
+			}
+		}
+}
