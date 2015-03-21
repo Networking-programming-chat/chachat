bc38992a5ced13288ba81c854f9469aeb561d698
campusCoder <campusCoder@users.noreply.github.com>
2015-03-21 03:27:06 +0200

    an updated client.h file, works too

--- client_side/client.h
+++ client_side/client.h
@@ -1,18 +1,36 @@
-#ifndef COMMON_H  //This is a preprocessor technique of preventing a header file from being included multiple times, which can be problematic for various reasons.
-#define COMMON_H
-
+#ifndef CLIENT_H 
+#define CLIENT_H
 
 #include <sys/socket.h>
+#include "../networking/netproto.h"
+#include <stdio.h>
+#include <sys/socket.h> //socket.
+#include <strings.h> //bzero
+#include <netdb.h> // addrinfo
+#include <unistd.h> //close
+#include <unistd.h>  
+#include <fcntl.h> 
 
-//
+#define MAX 50
+
 //client function:
-// create a socket
-// connection to the server
-//
-//argu: char *host is the string of server's name
-//     char *serv is the port of server in string format
-//return: the sock desciptor
+int client(const char *servName, const char *servPort);
+
+//prints chat rules and help
+int printChatRule(char *getCmd, char *getName);
+
+//handles the client's nickname
+int client_nick(int sockfd, char *nickname);
+
+//handles client's connection to the server
 int client_connect(const char *servName, const char *servPort);
 
+//Handling client's normal message
+void chatMessageHandle(int connfd, char *send, char *recv, char *getCmd, Msgheader chatheader);
 
+//Handling client's channel join command message
+void chanMessageHandle(int connfd, char *send, char *recv, char *getCmd, Msgheader chatheader);
+
+//Handling client's quite command message
+void quitMessageHandle(int connfd,char *mesbuff, Msgheader chatheader);
 #endif
