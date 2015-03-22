4bb6caec22e4d16d4fda30f32e62647273a69b46
McDevon <jussi.enroos@saunalahti.fi>
2015-03-22 13:40:05 +0200

    Function declarations for communication between client connection threads

---
+++ server_side/msg_buffers.c
@@ -0,0 +1,9 @@
+//
+//  msg_buffers.c
+//  Network
+//
+//  Created by Jussi Enroos on 22.3.2015.
+//  Copyright (c) 2015 Jussi Enroos. All rights reserved.
+//
+
+#include "msg_buffers.h"
---
+++ server_side/msg_buffers.h
@@ -0,0 +1,14 @@
+//
+//  msg_buffers.h
+//  Network
+//
+//  Created by Jussi Enroos on 22.3.2015.
+//  Copyright (c) 2015 Jussi Enroos. All rights reserved.
+//
+
+#ifndef __Network__msg_buffers__
+#define __Network__msg_buffers__
+
+#include <stdio.h>
+
+#endif /* defined(__Network__msg_buffers__) */
